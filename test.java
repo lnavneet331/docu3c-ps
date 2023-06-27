a4c32f62d5780b099f0cd25d36db0baaeaa34525
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-06-27 12:51:23 +0530

    Complete Problem Statement 3

---
+++ test.java
@@ -0,0 +1,21 @@
+// give me a code to give fibonacci series
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms: ");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        System.out.println("Fibonacci series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
